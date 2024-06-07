f7367476f8e6b499080a6f69c955a22840dcbdc8
Chris <stefaniakchristopher@gmail.com>
2024-06-06 21:40:55 -0400

    home page done

--- graph-builder-frontend/src/components/HomePage.js
+++ graph-builder-frontend/src/components/HomePage.js
@@ -2,109 +2,170 @@ import React from "react";
 import { FaChevronRight } from "react-icons/fa";
 import { IoStar } from "react-icons/io5";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
+import StepsPicture from "../assets/stepsgraphcrafter.png";
 
 const HomePage = () => {
   return (
     <>
-      <main>
-        <h1>GRAPH CRAFTER</h1>
-        <h3>
-          <span>Visualize</span> data in <span>seconds</span>
+      <main className="flex flex-col justify-center items-center h-[1000px]">
+        <h1 className=" text-9xl font-extrabold [color:var(--font-primary)] mb-4">
+          GRAPH CRAFTER
+        </h1>
+        <h3 className="text-5xl [color:var(--font-secondary)] mb-10">
+          <span className="[color:var(--backup-color)]">Visualize</span> data in{" "}
+          <span className="[color:var(--backup-color)] underline">seconds</span>
         </h3>
-        <button>
-          Get Started <FaChevronRight />
-        </button>
+        <Link to="/creategraph">
+          <button className="flex items-center [background:var(--backup-color)] py-4 px-10 rounded-xl font-bold [color:var(--font-secondary)] hover:bg-red-400 duration-300">
+            GET STARTED <FaChevronRight />
+          </button>
+        </Link>
       </main>
-      <section>
-        <h2>Features</h2>
-        <div>
-          <div>
-            <h3>Graph Types</h3>
-            <p>Choose from a variety of graph types to visualize your data.</p>
+      <section className="mb-20">
+        <hr className=" h-2 mx-[100px] [color:var(--font-secondary)] mb-12"></hr>
+        <h2 className="[color:var(--font-primary)] text-6xl ml-8 underline font-bold mb-20">
+          Features
+        </h2>
+        <div className="flex justify-evenly">
+          <div className=" rounded-full [background:var(--bg-secondary)] size-64 flex justify-center items-center flex-col">
+            <h3 className="[color:var(--font-primary)] text-4xl font-bold mb-4">
+              Graph Types
+            </h3>
+            <p className="text-center [color:var(--font-secondary)] font-semibold">
+              Choose from a variety of graph types to visualize your data.
+            </p>
           </div>
-          <div>
-            <h3>Easy to use</h3>
-            <p>
+          <div className=" rounded-full [background:var(--bg-secondary)] size-64 flex justify-center items-center flex-col">
+            <h3 className="[color:var(--font-primary)] text-4xl font-bold mb-4">
+              Easy to use
+            </h3>
+            <p className="text-center [color:var(--font-secondary)] font-semibold">
               Create your own graph in seconds by filling out a simple form.
             </p>
           </div>
-          <div>
-            <h3>Save Graphs</h3>
-            <p>Save your graphs to your account and view them later.</p>
+          <div className=" rounded-full [background:var(--bg-secondary)] size-64 flex justify-center items-center flex-col">
+            <h3 className="[color:var(--font-primary)] text-4xl font-bold mb-4">
+              Save Graphs
+            </h3>
+            <p className="text-center [color:var(--font-secondary)] font-semibold">
+              Save your graphs to your account and view them later.
+            </p>
           </div>
         </div>
       </section>
-      <section>
-        <h2>How to use</h2>
-        <div>
-          <img></img>
-        </div>
-        <div>
+      <section className="mb-20">
+        <hr className=" h-2 mx-[100px] [color:var(--font-secondary)] mb-12"></hr>
+        <h2 className="[color:var(--font-primary)] text-6xl ml-8 underline font-bold mb-20">
+          How to use
+        </h2>
+        <div className="flex px-10 justify-between">
           <div>
-            <h3>Step 1</h3>
-            <p>Choose labels and title</p>
+            <img
+              className=" border-2"
+              src={StepsPicture}
+              alt="Steps illustration"
+            />
           </div>
-          <div>
-            <h3>Step 2</h3>
-            <p>Choose graph type</p>
-          </div>
-          <div>
-            <h3>Step 3</h3>
-            <p>Fill out the data</p>
-          </div>
-          <div>
-            <h3>Step 4</h3>
-            <p>Create the graph</p>
+          <div className="flex flex-col  justify-center mr-60">
+            <div>
+              <h3 className="[color:var(--font-primary)] text-6xl mb-4">
+                Step 1
+              </h3>
+              <p className="[color:var(--font-secondary)] text-xl mb-8">
+                Choose labels and title
+              </p>
+            </div>
+            <div>
+              <h3 className="[color:var(--font-primary)] text-6xl mb-4">
+                Step 2
+              </h3>
+              <p className="[color:var(--font-secondary)] text-xl mb-8">
+                Choose graph type
+              </p>
+            </div>
+            <div>
+              <h3 className="[color:var(--font-primary)] text-6xl mb-4">
+                Step 3
+              </h3>
+              <p className="[color:var(--font-secondary)] text-xl mb-8">
+                Fill out the data
+              </p>
+            </div>
+            <div>
+              <h3 className="[color:var(--font-primary)] text-6xl mb-4">
+                Step 4
+              </h3>
+              <p className="[color:var(--font-secondary)] text-xl">
+                Create the graph
+              </p>
+            </div>
           </div>
         </div>
       </section>
-      <section>
-        <h2>Recent Reviews</h2>
-        <div>
-          <div>
-            <h4>Review 1</h4>
-            <div>
+      <section className="mb-20">
+        <hr className=" h-2 mx-[100px] [color:var(--font-secondary)] mb-12"></hr>
+        <h2 className="[color:var(--font-primary)] text-6xl ml-8 underline font-bold mb-20">
+          Recent Reviews
+        </h2>
+        <div className="flex justify-evenly">
+          <div className="w-[300px] p-4 border-2">
+            <h4 className="[color:var(--font-primary)] text-2xl font-semibold mb-1">
+              OMG it so cool!
+            </h4>
+            <div className="flex text-yellow-500 gap-2 mb-1">
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
             </div>
-            <p>
+            <p className="[color:var(--font-secondary)]">
               "This is the best graphing tool I have ever used. It is so easy to
               use and the graphs look amazing!"
             </p>
-            <p>- Jack Wellington</p>
-          </div>
-          <div>
-            <h4>Review 2</h4>
-            <div>
-              <IoStar />
-              <IoStar />
-              <IoStar />
-              <IoStar />
-              <IoStar />
-            </div>
-            <p>
-              "This is the best graphing tool I have ever used. It is so easy to
-              use and the graphs look amazing!"
+            <p className="[color:var(--font-secondary)] font-semibold">
+              - Jack Wellington
             </p>
           </div>
-          <p>- Jack Wellington</p>
-          <div>
-            <h4>Review 3</h4>
-            <div>
+          <div className="w-[300px] p-4 border-2">
+            <h4 className="[color:var(--font-primary)] text-2xl font-semibold mb-1">
+              CHANGED my LIFE
+            </h4>
+            <div className="flex text-yellow-500 gap-2 mb-1">
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
               <IoStar />
             </div>
-            <p>
+            <p className="[color:var(--font-secondary)]">
               "This is the best graphing tool I have ever used. It is so easy to
               use and the graphs look amazing!"
             </p>
-            <p>- Jack Wellington</p>
+            <p className="[color:var(--font-secondary)] font-semibold">
+              - Jack Wellington
+            </p>
+          </div>
+
+          <div className="w-[300px] p-4 border-2">
+            <h4 className="[color:var(--font-primary)] text-2xl font-semibold mb-1">
+              LOVE it more than my WIFE
+            </h4>
+            <div className="flex text-yellow-500 gap-2 mb-1">
+              <IoStar />
+              <IoStar />
+              <IoStar />
+              <IoStar />
+              <IoStar />
+            </div>
+            <p className="[color:var(--font-secondary)]">
+              "This is the best graphing tool I have ever used. It is so easy to
+              use and the graphs look amazing!"
+            </p>
+            <p className="[color:var(--font-secondary)] font-semibold">
+              - Jack Wellington
+            </p>
           </div>
         </div>
       </section>
