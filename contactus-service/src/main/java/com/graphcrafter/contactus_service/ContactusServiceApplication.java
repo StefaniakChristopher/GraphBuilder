6e74df3586697e1b4595370dbeb2a61c8b2c08ff
Chris <stefaniakchristopher@gmail.com>
2024-07-02 15:39:15 -0400

    contactus-service implemented

---
+++ contactus-service/src/main/java/com/graphcrafter/contactus_service/ContactusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.graphcrafter.contactus_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactusServiceApplication.class, args);
+	}
+
+}
---
+++ contactus-service/src/test/java/com/graphcrafter/contactus_service/ContactusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphcrafter.contactus_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactusServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
