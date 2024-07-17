c20cecb5d96b71e423e8827e479984b810a54f78
Chris <stefaniakchristopher@gmail.com>
2024-07-17 00:26:47 -0400

    working contactus endpoint, broken authentication

--- contactus-service/src/main/java/com/graphcrafter/contactus_service/RESTController.java
+++ contactus-service/src/main/java/com/graphcrafter/contactus_service/RESTController.java
@@ -3,6 +3,7 @@ package com.graphcrafter.contactus_service;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,9 @@ import jakarta.validation.Valid;
 
 @RestController
 public class RESTController {
+
+    @Autowired
+    private HandleMail sendMail;
 
     AtomicLong id_counter = new AtomicLong(0);
     HashMap<Integer, Message> messageHashMap = new HashMap<>();
@@ -24,6 +28,8 @@ public class RESTController {
         message.setId((int) newID);
 
         messageHashMap.put((int) newID, message);
+
+        sendMail.sendEmail(message);
 
         return ResponseEntity.ok(null);
     }
