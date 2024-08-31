867e690553e75b8c07f77910bc23805af69b4790
Chris <stefaniakchristopher@gmail.com>
2024-08-31 13:25:25 -0400

    contact us page responsive

--- graph-builder-frontend/src/components/ContactPage.js
+++ graph-builder-frontend/src/components/ContactPage.js
@@ -51,23 +51,23 @@ const ContactPage = () => {
 
   return (
     <div className="h-screen flex justify-center items-center">
-      <main className=" bg-slate-800 rounded-lg p-10 flex w-[1000px] h-[400px]">
-        <div className="text-white text-xl">
-          <h1 className="[color:var(--font-secondary)] text-7xl mb-6">
+      <main className=" bg-slate-800 rounded-lg p-10 px-14 mx-6 flex 2xl:flex-row flex-col justify-evenly">
+        <div className="text-white text-base md:text-2xl flex flex-col items-center 2xl:items-start 2xl:pr-10 pr-0">
+          <h1 className="[color:var(--font-secondary)] text-6xl md:text-8xl mb-6">
             Contact Us
           </h1>
           <p className="mb-2">Email: support@graphcrafter.com</p>
           <p className="mb-2">Phone: (123) 456-7890</p>
           <p className="mb-2">Address: 123 Graph St, Data City, DS 45678</p>
         </div>
-        <div className="flex justify-center w-1/2">
+        <div className="flex justify-center">
           <form
             onSubmit={handleSubmit}
-            className="flex flex-col items-start pt-4"
+            className="flex flex-col items-center pt-4 p-8 2xl:items-start"
           >
             <input
               placeholder="Name"
-              className="mb-3 w-[350px] p-2 rounded-lg"
+              className="mb-3 w-[300px] md:w-[400px] p-2 rounded-lg"
               name="name"
               value={consolidatedMessage.name}
               onChange={handleChange}
@@ -75,7 +75,7 @@ const ContactPage = () => {
             />
             <input
               placeholder="Email"
-              className="mb-3 w-[350px] p-2 rounded-lg"
+              className="mb-3 w-[300px] md:w-[400px] p-2 rounded-lg"
               name="email"
               value={consolidatedMessage.email}
               onChange={handleChange}
@@ -83,7 +83,7 @@ const ContactPage = () => {
             />
             <textarea
               placeholder="Message"
-              className="w-[350px] p-2 rounded-lg h-[150px]"
+              className="w-[300px] md:w-[400px] p-2 rounded-lg h-[150px]"
               name="message"
               value={consolidatedMessage.message}
               onChange={handleChange}
@@ -96,12 +96,12 @@ const ContactPage = () => {
               >
                 Submit
               </button>
-              <p className="mt-1">
+              <div className="h-6 flex items-center">
                 {loading && (
-                  <div className="spinner border-t-4 border-blue-500 rounded-full w-6 h-6 animate-spin"></div>
+                  <div className="spinner border-t-4 border-blue-500 rounded-full h-6 w-6 animate-spin"></div>
                 )}
                 <p
-                  className={`${
+                  className={` text-xs md:text-base ${ 
                     resultMessage.includes("Message failed to send")
                       ? "text-red-500"
                       : "text-green-400"
@@ -114,7 +114,7 @@ const ContactPage = () => {
                     </span>
                   ))}
                 </p>
-              </p>
+              </div>
             </div>
           </form>
         </div>
